56518f1310d8bf4ad23f45e77bdeedd6eb96a833
Martin Algesten <martin@algesten.se>
2022-08-07 12:50:04 +0200

    Test progress and host in common

--- ice/tests/common.rs
+++ ice/tests/common.rs
@@ -13,8 +13,41 @@ pub fn init_log() {
 }
 
 use std::net::SocketAddr;
+use std::time::Instant;
+
+use ice::{Candidate, IceAgent};
+use net::Receive;
+use tracing::Span;
 
 pub fn sock(s: impl Into<String>) -> SocketAddr {
     let s: String = s.into();
     s.parse().unwrap()
+}
+
+pub fn host(s: impl Into<String>) -> Candidate {
+    Candidate::host(sock(s)).unwrap()
+}
+
+pub fn progress(now: Instant, f: &mut IceAgent, t: &mut IceAgent, sf: &Span, st: &Span) -> Instant {
+    if let Some(trans) = sf.in_scope(|| f.poll_transmit()) {
+        println!("forward: {} -> {}", trans.source, trans.destination);
+        st.in_scope(|| t.handle_receive(now, Receive::try_from(&trans).unwrap()));
+    } else {
+        st.in_scope(|| t.handle_timeout(now));
+    }
+
+    let tim_f = sf.in_scope(|| f.poll_timeout());
+    let tim_t = st.in_scope(|| t.poll_timeout());
+
+    while let Some(v) = sf.in_scope(|| f.poll_event()) {
+        println!("Polled event: {:?}", v);
+        use ice::IceAgentEvent::*;
+        st.in_scope(|| match v {
+            IceRestart(v) => t.set_remote_credentials(v),
+            NewLocalCandidate(v) => t.add_remote_candidate(v),
+            _ => {}
+        });
+    }
+
+    tim_f.unwrap_or(now).min(tim_t.unwrap_or(now))
 }
--- ice/tests/host-host.rs
+++ ice/tests/host-host.rs
@@ -1,40 +1,10 @@
-use std::convert::TryFrom;
 use std::time::Instant;
 
-use common::{init_log, sock};
-use ice::{Candidate, IceAgent, IceAgentStats};
-use net::Receive;
-use tracing::{info_span, Span};
+use ice::{IceAgent, IceAgentStats};
+use tracing::info_span;
 
 mod common;
-
-fn host(s: impl Into<String>) -> Candidate {
-    Candidate::host(sock(s)).unwrap()
-}
-
-pub fn progress(now: Instant, f: &mut IceAgent, t: &mut IceAgent, sf: &Span, st: &Span) -> Instant {
-    if let Some(trans) = sf.in_scope(|| f.poll_transmit()) {
-        println!("forward: {} -> {}", trans.source, trans.destination);
-        st.in_scope(|| t.handle_receive(now, Receive::try_from(&trans).unwrap()));
-    } else {
-        st.in_scope(|| t.handle_timeout(now));
-    }
-
-    let tim_f = sf.in_scope(|| f.poll_timeout());
-    let tim_t = st.in_scope(|| t.poll_timeout());
-
-    while let Some(v) = sf.in_scope(|| f.poll_event()) {
-        println!("Polled event: {:?}", v);
-        use ice::IceAgentEvent::*;
-        st.in_scope(|| match v {
-            IceRestart(v) => t.set_remote_credentials(v),
-            NewLocalCandidate(v) => t.add_remote_candidate(v),
-            _ => {}
-        });
-    }
-
-    tim_f.unwrap_or(now).min(tim_t.unwrap_or(now))
-}
+use common::{host, init_log, progress};
 
 #[test]
 pub fn host_host() {
