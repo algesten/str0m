d537ba953cfe7bdcc0cfac4c3ab0a7a72afa63ee
Martin Algesten <martin@algesten.se>
2023-02-27 14:57:07 +0100

    Media::write drive time forwards

--- str0m/examples/chat.rs
+++ str0m/examples/chat.rs
@@ -536,9 +536,10 @@ impl Client {
             return;
         };
 
-        if let Err(e) = media
-            .writer(pt)
-            .write(data.network_time, data.time, &data.data)
+        if let Err(e) =
+            media
+                .writer(pt, Instant::now())
+                .write(data.network_time, data.time, &data.data)
         {
             warn!("Client ({}) failed: {:?}", *self.id, e);
             self.rtc.disconnect();
