39e20f24398c1f0c08bf2553e8fd479d40d0cfc7
Marcus Asteborg <maastebo@microsoft.com>
2023-03-23 21:25:50 +0000

    add api for local_dtls_fingerprint

--- src/dtls/mod.rs
+++ src/dtls/mod.rs
@@ -48,6 +48,43 @@ pub struct Fingerprint {
     pub bytes: Vec<u8>,
 }
 
+impl ToString for Fingerprint {
+    /// Convert to the hex string you find in SDP
+    fn to_string(&self) -> String {
+        format!(
+            "{} {}",
+            self.hash_func,
+            self.bytes
+                .iter()
+                .map(|b| format!("{:02X}", b))
+                .collect::<Vec<_>>()
+                .join(":")
+        )
+    }
+}
+
+impl std::str::FromStr for Fingerprint {
+    type Err = String;
+
+    fn from_str(hex_string: &str) -> Result<Self, Self::Err> {
+        let (hash_func, hex_with_colons) = hex_string
+            .split_once(' ')
+            .ok_or_else(|| "Failed to split once".to_owned())?;
+
+        let mut bytes = Vec::new();
+        for hex in hex_with_colons.split(':') {
+            let byte = u8::from_str_radix(hex, 16)
+                .map_err(|e| format!("Failed to parse fingerprint: {}", e))?;
+            bytes.push(byte);
+        }
+
+        Ok(Self {
+            hash_func: hash_func.to_owned(),
+            bytes,
+        })
+    }
+}
+
 /// Encapsulation of DTLS.
 pub struct Dtls {
     /// Context belongs together with Fingerprint.
