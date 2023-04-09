68d5a2636e6f64d537674fde65785762da29be11
xnorpx <xnorpx@outlook.com>
2023-04-09 14:23:06 -0700

    clappit

--- examples/tokio/reflect_server.rs
+++ examples/tokio/reflect_server.rs
@@ -1,13 +1,26 @@
+use clap::Parser;
 use tokio::signal;
 use tracing::{info, level_filters::LevelFilter};
 use util::init_log;
 
 mod util;
 
+#[derive(Parser, Debug)]
+struct Cli {
+    /// Http port
+    #[clap(default_value_t = 8081)]
+    http_port: usize,
+    /// Udp start port
+    #[clap(default_value_t = 30000)]
+    udp_start_port: usize,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     init_log(LevelFilter::INFO)?;
     info!("Starting reflect server");
+    let cli = Cli::parse();
+    info!("cli: {:?}", cli);
 
     signal::ctrl_c().await?;
     info!("Ctrl-C received, shutting down");
--- examples/tokio/text_chat_client.rs
+++ examples/tokio/text_chat_client.rs
@@ -1,13 +1,23 @@
+use clap::Parser;
 use tokio::signal;
 use tracing::{info, level_filters::LevelFilter};
 use util::init_log;
 
 mod util;
 
+#[derive(Parser, Debug)]
+struct Cli {
+    /// Server address
+    #[clap(default_value = "127.0.0.1:8081")]
+    server_addr: String,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     init_log(LevelFilter::INFO)?;
     info!("Starting chat client");
+    let cli = Cli::parse();
+    info!("cli: {:?}", cli);
 
     signal::ctrl_c().await?;
     info!("Ctrl-C received, shutting down");
