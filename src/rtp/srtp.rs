c6438ada6f99930c24ff92bace57df9015b2b95d
Martin Algesten <martin@algesten.se>
2023-08-21 22:47:12 +0200

    Update srtp.rs

--- src/rtp/srtp.rs
+++ src/rtp/srtp.rs
@@ -1099,7 +1099,7 @@ mod test {
         mod rfc7714 {
             // Test vectors from RFC7714
 
-            // Session Key(RTP and RTCP)
+            // Session Key (RTP and RTCP)
             pub(super) const KEY: [u8; 16] = [
                 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, //
                 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
