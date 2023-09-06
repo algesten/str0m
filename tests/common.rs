583e343f14beef72318d1399baa2a69a87b87a9e
Martin Algesten <martin@algesten.se>
2023-09-06 12:19:22 +0200

    Use real VP8 data to test contiguous

--- tests/common.rs
+++ tests/common.rs
@@ -1,14 +1,18 @@
 #![allow(unused)]
+use std::io::Cursor;
 use std::net::Ipv4Addr;
 use std::ops::{Deref, DerefMut};
 use std::sync::Once;
 use std::time::{Duration, Instant};
 
+use pcap_file::pcap::PcapReader;
 use rand::Rng;
 use str0m::change::SdpApi;
 use str0m::format::Codec;
 use str0m::format::PayloadParams;
 use str0m::net::Receive;
+use str0m::rtp::ExtensionMap;
+use str0m::rtp::RtpHeader;
 use str0m::Candidate;
 use str0m::{Event, Input, Output, Rtc, RtcError};
 use tracing::info_span;
@@ -245,4 +249,34 @@ pub fn connect_l_r() -> (TestRtc, TestRtc) {
     }
 
     (l, r)
+}
+
+pub fn vp8_data() -> Vec<(Duration, RtpHeader, Vec<u8>)> {
+    let reader = Cursor::new(include_bytes!("data/vp8.pcap"));
+    let mut r = PcapReader::new(reader).expect("vp8 pcap reader");
+
+    let exts = ExtensionMap::standard();
+
+    let mut ret = vec![];
+
+    let mut first = None;
+
+    while let Some(pkt) = r.next_packet() {
+        let pkt = pkt.unwrap();
+
+        if first.is_none() {
+            first = Some(pkt.timestamp);
+        }
+        let relative_time = pkt.timestamp - first.unwrap();
+
+        // This magic number 42 is the ethernet/IP/UDP framing of the packet.
+        let rtp_data = &pkt.data[42..];
+
+        let header = RtpHeader::parse(rtp_data, &exts).unwrap();
+        let payload = &rtp_data[header.header_len..];
+
+        ret.push((relative_time, header, payload.to_vec()));
+    }
+
+    ret
 }
---
+++ tests/contiguous.rs
@@ -0,0 +1,203 @@
+use std::net::Ipv4Addr;
+use std::time::Duration;
+
+use str0m::format::Codec;
+use str0m::media::{Direction, MediaKind};
+use str0m::Rtc;
+use str0m::{Candidate, Event, RtcError};
+use tracing::info_span;
+
+mod common;
+use common::vp8_data;
+use common::{init_log, progress, TestRtc};
+
+#[test]
+pub fn contiguous_all_the_way() -> Result<(), RtcError> {
+    init_log();
+
+    let mut l = TestRtc::new(info_span!("L"));
+    let mut r = TestRtc::new(info_span!("R"));
+
+    let host1 = Candidate::host((Ipv4Addr::new(1, 1, 1, 1), 1000).into())?;
+    let host2 = Candidate::host((Ipv4Addr::new(2, 2, 2, 2), 2000).into())?;
+    l.add_local_candidate(host1);
+    r.add_local_candidate(host2);
+
+    // The change is on the L (sending side) with Direction::SendRecv.
+    let mut change = l.sdp_api();
+    let mid = change.add_media(MediaKind::Video, Direction::SendOnly, None, None);
+    let (offer, pending) = change.apply().unwrap();
+
+    let answer = r.rtc.sdp_api().accept_offer(offer)?;
+    l.rtc.sdp_api().accept_answer(pending, answer)?;
+
+    loop {
+        if l.is_connected() || r.is_connected() {
+            break;
+        }
+        progress(&mut l, &mut r)?;
+    }
+
+    let max = l.last.max(r.last);
+    l.last = max;
+    r.last = max;
+
+    let params = l.params_vp8();
+    assert_eq!(params.spec().codec, Codec::Vp8);
+    let pt = params.pt();
+
+    let data = vp8_data();
+
+    for (relative, header, payload) in data {
+        // Keep RTC time progressed to be "in sync" with the test data.
+        while (l.last - max) < relative {
+            progress(&mut l, &mut r)?;
+        }
+
+        let absolute = max + relative;
+
+        let mut direct = l.direct_api();
+        let tx = direct.stream_tx_by_mid(mid, None).unwrap();
+        tx.write_rtp(
+            pt,
+            header.sequence_number(None),
+            header.timestamp,
+            absolute,
+            header.marker,
+            header.ext_vals,
+            true,
+            payload,
+        )
+        .unwrap();
+
+        progress(&mut l, &mut r)?;
+
+        if l.duration() > Duration::from_secs(5) {
+            break;
+        }
+    }
+
+    let iter = r.events.iter().filter_map(|(_, e)| {
+        if let Event::MediaData(d) = e {
+            Some(d)
+        } else {
+            None
+        }
+    });
+
+    let mut count = 0;
+
+    // Contiguous all the way through.
+    for data in iter {
+        println!("{:?} {:?}", data, data.seq_range);
+        assert!(data.contiguous);
+        count += 1;
+    }
+
+    // The last packet, 14362, is never flushed out because the depacketizer wants
+    // to see the next packet before releasing.
+    // We have 3 continuations and one last packet missing: 104 - 3 - 1 == 100
+    assert_eq!(count, 100);
+
+    Ok(())
+}
+
+#[test]
+pub fn not_contiguous() -> Result<(), RtcError> {
+    init_log();
+
+    let mut l = TestRtc::new(info_span!("L"));
+
+    // We need to lower the default reordering buffer size, or we won't make it
+    // past the dropped packet.
+    let rtc_r = Rtc::builder().set_reordering_size_video(5).build();
+
+    let mut r = TestRtc::new_with_rtc(info_span!("R"), rtc_r);
+
+    let host1 = Candidate::host((Ipv4Addr::new(1, 1, 1, 1), 1000).into())?;
+    let host2 = Candidate::host((Ipv4Addr::new(2, 2, 2, 2), 2000).into())?;
+    l.add_local_candidate(host1);
+    r.add_local_candidate(host2);
+
+    // The change is on the L (sending side) with Direction::SendRecv.
+    let mut change = l.sdp_api();
+    let mid = change.add_media(MediaKind::Video, Direction::SendOnly, None, None);
+    let (offer, pending) = change.apply().unwrap();
+
+    let answer = r.rtc.sdp_api().accept_offer(offer)?;
+    l.rtc.sdp_api().accept_answer(pending, answer)?;
+
+    loop {
+        if l.is_connected() || r.is_connected() {
+            break;
+        }
+        progress(&mut l, &mut r)?;
+    }
+
+    let max = l.last.max(r.last);
+    l.last = max;
+    r.last = max;
+
+    let params = l.params_vp8();
+    assert_eq!(params.spec().codec, Codec::Vp8);
+    let pt = params.pt();
+
+    let data = vp8_data();
+
+    for (relative, header, payload) in data {
+        // Drop a random packet in the middle.
+        if header.sequence_number == 14337 {
+            continue;
+        }
+
+        // Keep RTC time progressed to be "in sync" with the test data.
+        while (l.last - max) < relative {
+            progress(&mut l, &mut r)?;
+        }
+
+        let absolute = max + relative;
+
+        let mut direct = l.direct_api();
+        let tx = direct.stream_tx_by_mid(mid, None).unwrap();
+        tx.write_rtp(
+            pt,
+            header.sequence_number(None),
+            header.timestamp,
+            absolute,
+            header.marker,
+            header.ext_vals,
+            true,
+            payload,
+        )
+        .unwrap();
+
+        progress(&mut l, &mut r)?;
+
+        if l.duration() > Duration::from_secs(5) {
+            break;
+        }
+    }
+
+    let iter = r.events.iter().filter_map(|(_, e)| {
+        if let Event::MediaData(d) = e {
+            Some(d)
+        } else {
+            None
+        }
+    });
+
+    let mut count = 0;
+
+    // Contiguous all the way through.
+    for data in iter {
+        count += 1;
+        // We dropped packet 14337, which means 14338 should not be contiguous.
+        let assume_contiguous = !data.seq_range.contains(&14338.into());
+        assert!(assume_contiguous == data.contiguous);
+    }
+
+    // We have 3 continuations, one missing packet, and one last packet missing: 104 - 3 - 1 - 1 == 99
+    assert_eq!(count, 99);
+
+    Ok(())
+}
