3db8972ae74cca8b045f05db67163aa93d991a95
Martin Algesten <martin@algesten.se>
2023-12-27 15:20:22 +0100

    Add link to comment

--- src/io/stun.rs
+++ src/io/stun.rs
@@ -374,7 +374,7 @@ pub struct Attributes<'a> {
     xor_mapped_address: Option<SocketAddr>, // 0x0020
     software: Option<&'a str>,              // 0x0022
     fingerprint: Option<u32>,               // crc32
-    priority: Option<u32>,                  // 0x0024
+    priority: Option<u32>,                  // 0x0024 https://tools.ietf.org/html/rfc8445
     use_candidate: bool,                    // 0x0025
     ice_controlled: Option<u64>,            // 0x8029
     ice_controlling: Option<u64>,           // 0x802a
