9ce99d3b4bcc5f32e4fe01d1ecd536e9c94b0298
Martin Algesten <martin@algesten.se>
2024-01-06 20:16:20 +0100

    NUM_CPU in run-fuzz

--- run-fuzz.sh
+++ run-fuzz.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# This is for macOS.
+# For linux use: NUM_CPUS=$(awk '/^processor/ {++n} END {print n+1}' /proc/cpuinfo)
+NUM_CPUS=$(sysctl -n hw.ncpu)
+
 if [ "$1" == "" ]; then
     cargo +nightly fuzz list
 else
-    cargo +nightly fuzz run $1 -- --stop-after-first-failure -max_len=200000
+    cargo +nightly fuzz run $1 --jobs $NUM_CPUS -- --stop-after-first-failure -max_len=200000
 fi
