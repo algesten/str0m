5f270622aefadc812cb60fa6b2a48dbec42c6d54
Thomas Eizinger <thomas@eizinger.io>
2024-01-21 10:06:43 -0800

    Document why we can unwrap

--- src/crypto.rs
+++ src/crypto.rs
@@ -3,7 +3,8 @@ pub fn sha1_hmac(key: &[u8], payloads: &[&[u8]]) -> [u8; 20] {
     use hmac::Mac;
     use sha1::Sha1;
 
-    let mut hmac = Hmac::<Sha1>::new_from_slice(key).unwrap();
+    let mut hmac =
+        Hmac::<Sha1>::new_from_slice(key).expect("`Hmac` normalizes the key to 20 bytes");
 
     for payload in payloads {
         hmac.update(payload);
