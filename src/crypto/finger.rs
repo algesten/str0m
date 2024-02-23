de0b276b234d83dd62d692a933f8619d30fa297e
Martin Algesten <martin@algesten.se>
2024-02-23 15:50:24 +0100

    Test for Fingerprint::to_string()

--- src/crypto/finger.rs
+++ src/crypto/finger.rs
@@ -51,3 +51,21 @@ impl std::str::FromStr for Fingerprint {
         })
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn fingerprint_format() {
+        let f = Fingerprint {
+            hash_func: "foo".to_string(),
+            bytes: vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+        };
+
+        assert_eq!(
+            f.to_string(),
+            "foo 00:01:02:03:04:05:06:07:08:09:0A:0B:0C:0D:0E:0F:10:11"
+        );
+    }
+}
