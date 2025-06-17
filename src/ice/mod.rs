ebce893a92d77e5a29920364e68318064d488d36
Martin Algesten <martin@algesten.se>
2025-06-17 15:06:04 +0200

    Re-enable disabled tests

--- src/ice/mod.rs
+++ src/ice/mod.rs
@@ -809,80 +809,80 @@ mod test {
         assert_eq!(a2.num_candidate_pairs(), 1);
     }
 
-    // // In general, ICE prefers IPv6 over IPv4.
-    // // However, in case our only IPv6 connectivity is via a relay that we are talking to over IPv4,
-    // // we want to prefer the IPv4 code path.
-    // #[test]
-    // fn prefers_ipv4_ipv4_relay_candidate_over_ipv4_ipv6_controlling() {
-    //     let _guard = tracing_subscriber::fmt()
-    //         .with_env_filter("debug")
-    //         .with_test_writer()
-    //         .set_default();
+    // In general, ICE prefers IPv6 over IPv4.
+    // However, in case our only IPv6 connectivity is via a relay that we are talking to over IPv4,
+    // we want to prefer the IPv4 code path.
+    #[test]
+    fn prefers_ipv4_ipv4_relay_candidate_over_ipv4_ipv6_controlling() {
+        let _guard = tracing_subscriber::fmt()
+            .with_env_filter("debug")
+            .with_test_writer()
+            .set_default();
 
-    //     let mut a1 = TestAgent::new(info_span!("L"));
-    //     let mut a2 = TestAgent::new(info_span!("R"));
+        let mut a1 = TestAgent::new(info_span!("L"));
+        let mut a2 = TestAgent::new(info_span!("R"));
 
-    //     a1.set_controlling(true);
-    //     a2.set_controlling(false);
+        a1.set_controlling(true);
+        a2.set_controlling(false);
 
-    //     prefer_ipv4_candidate_over_ipv6_candidate(&mut a1, &mut a2);
-    // }
+        prefer_ipv4_candidate_over_ipv6_candidate(&mut a1, &mut a2);
+    }
 
-    // // In general, ICE prefers IPv6 over IPv4.
-    // // However, in case our only IPv6 connectivity is via a relay that we are talking to over IPv4,
-    // // we want to prefer the IPv4 code path.
-    // #[test]
-    // fn prefers_ipv4_ipv4_relay_candidate_over_ipv4_ipv6_controlled() {
-    //     let _guard = tracing_subscriber::fmt()
-    //         .with_env_filter("debug")
-    //         .with_test_writer()
-    //         .set_default();
+    // In general, ICE prefers IPv6 over IPv4.
+    // However, in case our only IPv6 connectivity is via a relay that we are talking to over IPv4,
+    // we want to prefer the IPv4 code path.
+    #[test]
+    fn prefers_ipv4_ipv4_relay_candidate_over_ipv4_ipv6_controlled() {
+        let _guard = tracing_subscriber::fmt()
+            .with_env_filter("debug")
+            .with_test_writer()
+            .set_default();
 
-    //     let mut a1 = TestAgent::new(info_span!("L"));
-    //     let mut a2 = TestAgent::new(info_span!("R"));
+        let mut a1 = TestAgent::new(info_span!("L"));
+        let mut a2 = TestAgent::new(info_span!("R"));
 
-    //     a1.set_controlling(false);
-    //     a2.set_controlling(true);
+        a1.set_controlling(false);
+        a2.set_controlling(true);
 
-    //     prefer_ipv4_candidate_over_ipv6_candidate(&mut a1, &mut a2);
-    // }
+        prefer_ipv4_candidate_over_ipv6_candidate(&mut a1, &mut a2);
+    }
 
-    // fn prefer_ipv4_candidate_over_ipv6_candidate(a1: &mut TestAgent, a2: &mut TestAgent) {
-    //     // Agent 1 only has IPv4 connectivity to a relay but allocates both IPv4 and IPv6 addresses.
-    //     // Agent 2 has no relay but has full IPv4 and IPv6 connectivity.
-    //     let relay_ipv4_ipv4 = a1
-    //         .add_local_candidate(relay("7.7.7.7:5000", "udp"))
-    //         .unwrap()
-    //         .clone();
-    //     let relay_ipv6_ipv4 = a1
-    //         .add_local_candidate(relay("[::7]:5000", "udp"))
-    //         .unwrap()
-    //         .clone();
-    //     a2.add_remote_candidate(relay_ipv4_ipv4);
-    //     a2.add_remote_candidate(relay_ipv6_ipv4);
+    fn prefer_ipv4_candidate_over_ipv6_candidate(a1: &mut TestAgent, a2: &mut TestAgent) {
+        // Agent 1 only has IPv4 connectivity to a relay but allocates both IPv4 and IPv6 addresses.
+        // Agent 2 has no relay but has full IPv4 and IPv6 connectivity.
+        let relay_ipv4_ipv4 = a1
+            .add_local_candidate(relay("7.7.7.7:5000", "udp", "1.1.1.1:5000"))
+            .unwrap()
+            .clone();
+        let relay_ipv6_ipv4 = a1
+            .add_local_candidate(relay("[::7]:5000", "udp", "1.1.1.1:5000"))
+            .unwrap()
+            .clone();
+        a2.add_remote_candidate(relay_ipv4_ipv4);
+        a2.add_remote_candidate(relay_ipv6_ipv4);
 
-    //     let host_ipv4 = a2.add_host_candidate("5.5.5.5:3000");
-    //     let host_ipv6 = a2.add_host_candidate("[::2]:3000");
-    //     a1.add_remote_candidate(host_ipv4);
-    //     a1.add_remote_candidate(host_ipv6);
+        let host_ipv4 = a2.add_host_candidate("5.5.5.5:3000");
+        let host_ipv6 = a2.add_host_candidate("[::2]:3000");
+        a1.add_remote_candidate(host_ipv4);
+        a1.add_remote_candidate(host_ipv6);
 
-    //     // loop until we're connected.
-    //     loop {
-    //         if a1.state().is_connected() && a2.state().is_connected() {
-    //             break;
-    //         }
-    //         progress(a1, a2);
-    //     }
+        // loop until we're connected.
+        loop {
+            if a1.state().is_connected() && a2.state().is_connected() {
+                break;
+            }
+            progress(a1, a2);
+        }
 
-    //     assert!(a1.has_event(|e| {
-    //                 matches!(e, IceAgentEvent::NominatedSend { source, destination, .. }
-    //                      if source == &sock("7.7.7.7:5000") && destination == &sock("5.5.5.5:3000"))
-    //             }));
-    //     assert!(a2.has_event(|e| {
-    //                 matches!(e, IceAgentEvent::NominatedSend { source, destination, .. }
-    //                      if source == &sock("5.5.5.5:3000") && destination == &sock("7.7.7.7:5000"))
-    //             }));
-    // }
+        assert!(a1.has_event(|e| {
+            matches!(e, IceAgentEvent::NominatedSend { source, destination, .. }
+                         if source == &sock("7.7.7.7:5000") && destination == &sock("5.5.5.5:3000"))
+        }));
+        assert!(a2.has_event(|e| {
+            matches!(e, IceAgentEvent::NominatedSend { source, destination, .. }
+                         if source == &sock("5.5.5.5:3000") && destination == &sock("7.7.7.7:5000"))
+        }));
+    }
 
     #[test]
     fn changed_timing_config_takes_effect_immediately() {
