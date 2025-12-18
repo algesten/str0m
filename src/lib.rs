fe796f35eeea112111897c6ee79a5547fd682b65
Martin Algesten <martin@algesten.se>
2025-12-18 22:57:59 +0100

    Fix order of DTLS pending packet poll

--- src/lib.rs
+++ src/lib.rs
@@ -807,7 +807,6 @@ pub struct Rtc {
     dtls: Dtls,
     dtls_connected: bool,
     dtls_buf: Vec<u8>,
-    pending_dtls_packets: std::collections::VecDeque<net::DatagramSend>,
     next_dtls_timeout: Option<Instant>,
     sctp: RtcSctp,
     chan: ChannelHandler,
@@ -1124,7 +1123,6 @@ impl Rtc {
             .expect("DTLS to init without problem"),
             dtls_connected: false,
             dtls_buf: vec![0; 2000],
-            pending_dtls_packets: std::collections::VecDeque::new(),
             next_dtls_timeout: None,
             session,
             sctp: RtcSctp::new(),
@@ -1448,8 +1446,8 @@ impl Rtc {
         let mut just_connected = false;
         loop {
             match self.dtls.poll_output(&mut self.dtls_buf) {
-                DtlsOutput::Packet(data) => {
-                    self.pending_dtls_packets.push_back(data.to_vec().into());
+                DtlsOutput::Packet(_) => {
+                    unreachable!("We don't expect DTLS packets here since we use poll_packet");
                 }
                 DtlsOutput::Connected => {
                     if !self.dtls_connected {
@@ -1580,7 +1578,7 @@ impl Rtc {
         if let Some(send) = &self.send_addr {
             // These can only be sent after we got an ICE connection.
             let datagram = None
-                .or_else(|| self.pending_dtls_packets.pop_front())
+                .or_else(|| self.dtls.poll_packet())
                 .or_else(|| self.session.poll_datagram(self.last_now));
 
             if let Some(contents) = datagram {
