4ec45b89ee3e789dec1361d4f98aa6d34a61ffe7
Lukas Herman <lherman.cs@gmail.com>
2025-12-19 18:03:38 -0500

    feat: typesafe Candidate builder
    * exp: init candidate builder
    
    * add ssl_tcp and tls
    
    * add candidate builder test
    
    * move down builder
    
    * add some comments
    
    * remove host_tcp
    
    * add example and expose builder
    
    * add api result consistency
    
    * remove unnecessary checks

--- src/lib.rs
+++ src/lib.rs
@@ -665,7 +665,7 @@ use dtls::Dtls;
 mod ice_;
 use ice_::IceAgent;
 use ice_::IceAgentEvent;
-pub use ice_::{Candidate, CandidateKind, IceConnectionState, IceCreds};
+pub use ice_::{Candidate, CandidateBuilder, CandidateKind, IceConnectionState, IceCreds};
 
 #[path = "config.rs"]
 mod config_mod;
