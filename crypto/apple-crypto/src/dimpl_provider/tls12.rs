6ea5471a5f7d908f2d36d045450988dee6bdbf98
efer-ms <edufernandez@microsoft.com>
2026-01-11 22:38:19 -0800

    Add a test vector

--- crypto/apple-crypto/src/dimpl_provider/tls12.rs
+++ crypto/apple-crypto/src/dimpl_provider/tls12.rs
@@ -103,3 +103,65 @@ impl PrfProvider for ApplePrfProvider {
 }
 
 pub(super) static PRF_PROVIDER: ApplePrfProvider = ApplePrfProvider;
+
+#[cfg(test)]
+mod tests {
+    use dimpl::buffer::Buf;
+    use dimpl::crypto::HashAlgorithm;
+    use dimpl::crypto::PrfProvider;
+
+    /// Convert an ASCII hex array into a byte array at compile time.
+    macro_rules! hex_as_bytes {
+        ($input:expr) => {{
+            const fn from_hex_char(c: u8) -> u8 {
+                match c {
+                    b'0'..=b'9' => c - b'0',
+                    b'a'..=b'f' => c - b'a' + 10,
+                    b'A'..=b'F' => c - b'A' + 10,
+                    _ => panic!("Invalid hex character"),
+                }
+            }
+
+            const INPUT: &[u8] = $input;
+            const LEN: usize = INPUT.len();
+            const OUTPUT_LEN: usize = LEN / 2;
+
+            const fn convert() -> [u8; OUTPUT_LEN] {
+                assert!(LEN % 2 == 0, "Hex string length must be even");
+
+                let mut out = [0u8; OUTPUT_LEN];
+                let mut i = 0;
+                while i < LEN {
+                    out[i / 2] = (from_hex_char(INPUT[i]) << 4) | from_hex_char(INPUT[i + 1]);
+                    i += 2;
+                }
+                out
+            }
+
+            convert()
+        }};
+    }
+
+    #[test]
+    fn test_prf_tls12_sha256() {
+        // This test vector was found here: https://github.com/xomexh/TLS-PRF
+        let mut output = Buf::new();
+        let mut scratch = Buf::new();
+        let provider = super::ApplePrfProvider {};
+        assert!(provider
+            .prf_tls12(
+                &hex_as_bytes!(b"9bbe436ba940f017b17652849a71db35"),
+                "test label",
+                &hex_as_bytes!(b"a0ba9f936cda311827a6f796ffd5198c"),
+                &mut output,
+                100,
+                &mut scratch,
+                HashAlgorithm::SHA256,
+            )
+            .is_ok());
+        assert_eq!(
+            output.as_ref(),
+            &hex_as_bytes!(b"e3f229ba727be17b8d122620557cd453c2aab21d07c3d495329b52d4e61edb5a6b301791e90d35c9c9a46b4e14baf9af0fa022f7077def17abfd3797c0564bab4fbc91666e9def9b97fce34f796789baa48082d122ee42c5a72e5a5110fff70187347b66")
+        );
+    }
+}
