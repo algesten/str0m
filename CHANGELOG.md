d84d6e09727c221012ff7a4cc8044477041400c7
Lukas Herman <lherman.cs@gmail.com>
2026-05-26 22:41:42 +0800

    fix: abs-capture-time dropped on receiver

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 # Unreleased
 
+  * Add `abs-capture-time` writer support and fixed receiver-side extension propagation (breaking) #964
   * Expose ICE candidate pair RTT in CandidatePairStats #962
   * Add stereo to opus format params #955
   * Use `Arc<[u8]>` as input/output of media data (breaking) #959
--- src/media/writer.rs
+++ src/media/writer.rs
@@ -3,6 +3,7 @@ use std::time::Instant;
 
 use crate::RtcError;
 use crate::format::PayloadParams;
+use crate::rtp_::AbsCaptureTime;
 use crate::rtp_::MidRid;
 use crate::rtp_::VideoOrientation;
 use crate::session::Session;
@@ -95,6 +96,12 @@ impl<'a> Writer<'a> {
     /// whether the video requires to be rotated to show correctly.
     pub fn video_orientation(mut self, o: VideoOrientation) -> Self {
         self.ext_vals.video_orientation = Some(o);
+        self
+    }
+
+    /// Set absolute capture time for this frame.
+    pub fn abs_capture_time(mut self, capture_time: AbsCaptureTime) -> Self {
+        self.ext_vals.abs_capture_time = Some(capture_time);
         self
     }
 
